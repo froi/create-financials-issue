9d41c78710ab943193bbad6e21df3fe027a7835f
Froilan Irizarry <froi@github.com>
2020-01-24 13:12:57 -0400

    Initial changes - Added @actions/github import - Added issue creation - Added action setOutput to return issue number - Added getRequestParams helper function - Added getClient helper funtion
    Signed-off-by: Froilan Irizarry <froi@github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,37 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import {IssuesCreateParamsDeprecatedAssignee} from '@octokit/rest';
+
+function getClient(): github.GitHub {
+  const token: string = process.env.GITHUB_TOKEN || '';
+  return new github.GitHub(token);
+}
+function getRequestParams(): IssuesCreateParamsDeprecatedAssignee {
+  const title = core.getInput('title');
+  const assignees = core.getInput('assignees');
+  const body = core.getInput('body');
+  const labels = core.getInput('labels');
+
+  return {
+    assignees: assignees.split(','),
+    body,
+    labels: labels.split(','),
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    title
+  };
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const octokit: github.GitHub = getClient();
+    const issueParams: IssuesCreateParamsDeprecatedAssignee = getRequestParams();
+    const issue = await octokit.issues.create(issueParams);
+    core.info(`Issue created - Title: ${issue.data.title}, Number: ${issue.data.number}`);
+    core.setOutput('issue_number', `${issue.data.number}`);
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();

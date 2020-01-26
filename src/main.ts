7eba6d97b88eb85b870138368ab9257ef2ee081e
Froilan Irizarry <froi@github.com>
2020-01-26 14:13:12 -0400

    Moved helper functions to utils.ts
    Signed-off-by: Froilan Irizarry <froi@github.com>

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,6 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import {IssuesCreateParamsDeprecatedAssignee} from '@octokit/rest';
 
-function getClient(): github.GitHub {
-  const token: string = process.env.GITHUB_TOKEN || '';
-  return new github.GitHub(token);
-}
-function getRequestParams(): IssuesCreateParamsDeprecatedAssignee {
-  const title = core.getInput('title');
-  const assignees = core.getInput('assignees');
-  const body = core.getInput('body');
-  const labels = core.getInput('labels');
-
-  return {
-    assignees: assignees.split(','),
-    body,
-    labels: labels.split(','),
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    title
-  };
-}
-
 async function run(): Promise<void> {
   try {
     const octokit: github.GitHub = getClient();
